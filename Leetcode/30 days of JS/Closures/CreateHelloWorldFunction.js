690fd7bfe1fe2095ae967782a6ce72aa1eede8a4
RahulScripted <goswami.rahul1002@gmail.com>
2026-01-28 18:24:20 +0530

    Done

---
+++ Leetcode/30 days of JS/Closures/CreateHelloWorldFunction.js
@@ -0,0 +1,14 @@
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
+
+
+
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World"
+    }
+};
+
+const helloFn = createHelloWorld();
+console.log(helloFn());
